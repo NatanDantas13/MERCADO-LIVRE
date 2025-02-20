45d8822ee99f44838c6430342e86d9624809cb67
Natan Ramalho <natan.rsd13@gmail.com>
2025-02-20 20:17:15 -0300

    Adicionando controller exibição produtos

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  public mensagem: string = '';
+  public produtos: any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.mensagem = 'Bem-vindo ao meu componente!';
+    this.produtos = [
+      { id: 1, nome: 'Produto 1', descricao: 'Descrição do produto 1', preco: 10.99},
+      { id: 2, nome: 'Produto 2', descricao: 'Descrição do produto 2', preco: 56.99},
+    ];
+  }
   // Lógica do componente pode ser adicionada aqui
 }
